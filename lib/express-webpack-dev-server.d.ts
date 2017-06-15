f0d2c6fd872bcef159f3b5bf233d172905285981
Hugyák Tamás <tamas.hugyak@outlook.com>
2017-06-15 09:29:08 +0200

    yarn.lock file removed & TypeScript typings added

---
+++ lib/express-webpack-dev-server.d.ts
@@ -0,0 +1,20 @@
+
+/**
+ * @utf8 ő
+ */
+
+declare module "express-webpack-dev-server" {
+	import { Server }		from "http";
+	import { Application }	from "express";
+
+	function ExpressWebpackDevServer(
+		httpServer			: Server,
+		expressApp			: Application,
+		webpackConfig		: object,
+		middlewareOptions	: object
+	) : void;
+
+	namespace ExpressWebpackDevServer {}
+
+	export = ExpressWebpackDevServer;
+}
