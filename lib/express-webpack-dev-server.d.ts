2244808a8e65db7a9ab8d60f52f9ff156604b260
Hugyák Tamás <tamas.hugyak@outlook.com>
2017-06-15 15:12:38 +0200

    the export of class modified & declaration file fixed

--- lib/express-webpack-dev-server.d.ts
+++ lib/express-webpack-dev-server.d.ts
@@ -7,12 +7,15 @@ declare module "express-webpack-dev-server" {
 	import { Server }		from "http";
 	import { Application }	from "express";
 
-	function ExpressWebpackDevServer(
-		httpServer			: Server,
-		expressApp			: Application,
-		webpackConfig		: object,
-		middlewareOptions	: object
-	) : void;
-
-	namespace ExpressWebpackDevServer {}
+	class ExpressWebpackDevServer {
+		constructor(
+			httpServer			: Server,
+			expressApp			: Application,
+			webpackConfig		: object,
+			middlewareOptions	: object
+		);
+	}
+	export {
+		ExpressWebpackDevServer
+	};
 }
