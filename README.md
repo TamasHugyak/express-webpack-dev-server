e4ad6bce6a2e22bdcb89d3a37fdc57fc86f7d435
Tamás Hugyák <tamas.hugyak@gmail.com>
2018-04-30 15:32:59 +0200

    feat: add Webpack 4 support + use custom version of webpack-hot-client module

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # express-webpack-dev-server
 
-This is a plugin for Express web server to support Webpack 3 module bundler's live reload feature.
+This is a plugin for Express web server to support Webpack 4 module bundler's live reload feature.
 
-The repo contains files from [webpack-dev-server](https://github.com/webpack/webpack-dev-server) and [hapi-webpack-dev-server-plugin](https://github.com/atroo/hapi-webpack-dev-server-plugin) projects.
-
+The repo contains files from [webpack-hot-client](https://github.com/webpack-contrib/webpack-hot-client) project.
 
 ## LICENSE
 
--- lib/express-webpack-dev-server.d.ts
+++ lib/express-webpack-dev-server.d.ts
@@ -1,23 +1,25 @@
 
-import { Server }		from 'http';
-import { Application }	from 'express';
+import { Server }      from 'http';
+import { Application } from 'express';
 
 declare namespace w {
-	class ExpressWebpackDevServer {
-		constructor(
-			httpServer			: Server,
-			expressApp			: Application,
-			webpackConfig		: object,
-			middlewareOptions	: object
-		);
+  class ExpressWebpackDevServer {
+    constructor(
+      httpServer        : Server,
+      expressApp        : Application,
+      webpackConfig     : {[key: string]: any},
+      middlewareOptions : {[key: string]: any},
+      hotClientOptions  : {[key: string]: any}
+    );
 
-		new (
-			httpServer			: Server,
-			expressApp			: Application,
-			webpackConfig		: object,
-			middlewareOptions	: object
-		) : void;
-	}
+    new (
+      httpServer        : Server,
+      expressApp        : Application,
+      webpackConfig     : {[key: string]: any},
+      middlewareOptions : {[key: string]: any},
+      hotClientOptions  : {[key: string]: any}
+    ) : void;
+  }
 }
 
 export = w;
